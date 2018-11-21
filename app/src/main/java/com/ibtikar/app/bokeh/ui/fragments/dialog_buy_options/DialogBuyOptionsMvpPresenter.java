f3167484cc6259e80e2e18b7f8315765d6d5a3d0
Ibtikar100 <android@ibtikartechs.com>
2018-11-21 16:50:27 +0200

    new

--- app/src/main/java/com/ibtikar/app/bokeh/ui/fragments/dialog_buy_options/DialogBuyOptionsMvpPresenter.java
+++ app/src/main/java/com/ibtikar/app/bokeh/ui/fragments/dialog_buy_options/DialogBuyOptionsMvpPresenter.java
@@ -3,6 +3,6 @@ package com.ibtikar.app.bokeh.ui.fragments.dialog_buy_options;
 import com.ibtikar.app.bokeh.ui.activities.base.MvpPresenter;
 
 public interface DialogBuyOptionsMvpPresenter <V extends DialogBuyOptionsMvpView> extends MvpPresenter<V> {
-    void submitAndAddItem();
+    void submitAndAddItem(Integer deliveryOrPickup, Integer areaId, Integer cityId, String textAddress, String deliveryDate, String deliveryTime);
     void loadAreasSpinner();
 }
